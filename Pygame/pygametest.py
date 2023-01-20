5eaee30266db7af767c67bb0d51b7f9cfc5ab651
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-01-20 20:04:18 +0100

    The beginning is done but now what

--- Pygame/pygametest.py
+++ Pygame/pygametest.py
@@ -2,15 +2,26 @@ import pygame
 
 WIDTH, LENGTH = 900,500
 WIN = pygame.display.set_mode((WIDTH, LENGTH))
-pygame.display.set_caption("Minesweeper", icontitle=None)
+pygame.display.set_caption("Minesweeper")
+
+BG_COLOUR = (32, 25, 36)
+FPS = 60
+
+def draw():
+
+    WIN.fill(BG_COLOUR)
+    pygame.display.update()
 
 def main(): 
 
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
-            if event.type == pygame.quit:
+            if event.type == pygame.QUIT:
                 run = False
+        draw()
     pygame.quit()
 
 if __name__ == "__main__":
